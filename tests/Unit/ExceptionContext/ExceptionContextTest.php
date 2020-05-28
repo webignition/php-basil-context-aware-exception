0ab78e23ccdeb5ada1a254f2c3510b6726a2329a
Jon Cram <webignition@gmail.com>
2020-05-28 11:45:08 +0100

    Apply context values without overwriting exitisting (#7)

--- tests/Unit/ExceptionContext/ExceptionContextTest.php
+++ tests/Unit/ExceptionContext/ExceptionContextTest.php
@@ -3,6 +3,7 @@
 namespace webignition\BasilContextAwareException\Tests\Unit\ExceptionContext;
 
 use webignition\BasilContextAwareException\ExceptionContext\ExceptionContext;
+use webignition\BasilContextAwareException\ExceptionContext\ExceptionContextInterface;
 use webignition\BasilContextAwareException\Tests\DataProvider\ExceptionContextDataProviderTrait;
 
 class ExceptionContextTest extends \PHPUnit\Framework\TestCase
@@ -23,5 +24,87 @@ class ExceptionContextTest extends \PHPUnit\Framework\TestCase
         $this->assertSame($expectedTestName, $exceptionContext->getTestName());
         $this->assertSame($expectedStepName, $exceptionContext->getStepName());
         $this->assertSame($expectedContent, $exceptionContext->getContent());
+    }
+
+    /**
+     * @dataProvider applyDataProvider
+     *
+     * @param ExceptionContextInterface $exceptionContext
+     * @param array<string, string|null> $values
+     * @param ExceptionContextInterface $expectedExceptionContext
+     */
+    public function testApply(
+        ExceptionContextInterface $exceptionContext,
+        array $values,
+        ExceptionContextInterface $expectedExceptionContext
+    ) {
+        $exceptionContext->apply($values);
+
+        $this->assertEquals($expectedExceptionContext, $exceptionContext);
+    }
+
+    public function applyDataProvider(): array
+    {
+        return [
+            'empty context, empty values' => [
+                'exceptionContext' => new ExceptionContext(),
+                'values' => [],
+                'expectedExceptionContext' => new ExceptionContext(),
+            ],
+            'test name only context, empty values' => [
+                'exceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                ]),
+                'values' => [],
+                'expectedExceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                ]),
+            ],
+            'step name only context, empty values' => [
+                'exceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                ]),
+                'values' => [],
+                'expectedExceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                ]),
+            ],
+            'content only context, empty values' => [
+                'exceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_CONTENT => 'content',
+                ]),
+                'values' => [],
+                'expectedExceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_CONTENT => 'content',
+                ]),
+            ],
+            'full context, empty values' => [
+                'exceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                    ExceptionContextInterface::KEY_CONTENT => 'content',
+                ]),
+                'values' => [],
+                'expectedExceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                    ExceptionContextInterface::KEY_CONTENT => 'content',
+                ]),
+            ],
+            'content context, add test name and step name values' => [
+                'exceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_CONTENT => 'content',
+                ]),
+                'values' => [
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                ],
+                'expectedExceptionContext' => new ExceptionContext([
+                    ExceptionContextInterface::KEY_TEST_NAME => 'test name',
+                    ExceptionContextInterface::KEY_STEP_NAME => 'step name',
+                    ExceptionContextInterface::KEY_CONTENT => 'content',
+                ]),
+            ],
+        ];
     }
 }
